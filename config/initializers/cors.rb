d6aae7e994b9e58f61016332163a668209f77fe8
kavya <kavya.actimize@gmail.com>
2026-07-07 18:09:31 +0530

    add

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001"   # React port
+    origins "http://localhost:3001" ,
+    "https://task-17-wysf.onrender.com/"  # React port
 
     resource "*",
       headers: :any,
