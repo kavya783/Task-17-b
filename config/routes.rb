97c92cf679f0d521b63d1e57e89a5da3ebebfa9a
kavya <kavya.actimize@gmail.com>
2026-07-07 09:43:04 +0530

    config file

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  namespace :api do
+    post "signup", to: "auth#signup"
+    post "login", to: "auth#login"
+
+    resources :users, only: [:index, :create, :update, :destroy]
+    resources :leaves
+  end
+end
