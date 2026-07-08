e80e1b0212db3755ffc482e1e90f2c552629617e
kavya <kavya.actimize@gmail.com>
2026-07-08 14:28:18 +0530

    add

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -14,14 +14,17 @@ module Api
 end
 
     def create
-      user = User.new(user_params)
+  Rails.logger.info "PARAMS: #{params.inspect}"
+  Rails.logger.info "PROFILE IMAGE: #{params[:profile_image].inspect}"
 
-      if user.save
-        render json: user, status: :created
-      else
-        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-      end
-    end
+  user = User.new(user_params)
+ActiveStorage::Attachment.count
+  if user.save
+    render json: user, status: :created
+  else
+    render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+  end
+end
     def show
   user = User.find(params[:id])
   render json: user
