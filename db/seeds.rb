2dc857a15c225f802730243d6e7b69aee4c9eb9e
kavya <kavya.actimize@gmail.com>
2026-07-08 09:40:50 +0530

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,22 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 # AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-# AdminUser.find_or_initialize_by(email: "admin@gmail.com").tap do |admin|
-#   admin.password = "Admin@123"
-#   admin.password_confirmation = "Admin@123"
-#   admin.save!
-# end
-
-User.create(
-  name: "Hr",
-  email: "hr@gmail.com",
-  password: "test@123",
-  password_confirmation: "test@123"
-)
-
-User.find_or_initialize_by(email: "hr@gmail.com").tap do |userr|
-  userr.password = "Test@123"
-  userr.password_confirmation = "Test@123"
-  userr.save!
+AdminUser.find_or_initialize_by(email: "admin@gmail.com").tap do |admin|
+  admin.password = "Admin@123"
+  admin.password_confirmation = "Admin@123"
+  admin.save!
 end
+
+# User.create(
+#   name: "Hr",
+#   email: "hr@gmail.com",
+#   password: "test@123",
+#   password_confirmation: "test@123"
+# )
+
+# User.find_or_initialize_by(email: "hr@gmail.com").tap do |userr|
+#   userr.password = "Test@123"
+#   userr.password_confirmation = "Test@123"
+#   userr.save!
+# end
 
