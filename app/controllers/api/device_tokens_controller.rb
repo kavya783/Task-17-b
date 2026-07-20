5338cfb559fdfe3aeecf26d9a91fe5cbb7c014d7
kavya <kavya.actimize@gmail.com>
2026-07-20 09:29:01 +0530

    add

--- app/controllers/api/device_tokens_controller.rb
+++ app/controllers/api/device_tokens_controller.rb
@@ -2,26 +2,21 @@ class Api::DeviceTokensController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-
     device_token = DeviceToken.find_or_initialize_by(
-      user_id: params[:user_id],
-      token: params[:token]
+      user_id: params[:user_id]
     )
 
-    if device_token.save
+    device_token.token = params[:token]
 
+    if device_token.save
       render json: {
         message: "Token saved successfully",
         device_token: device_token
       }, status: :ok
-
     else
-
       render json: {
         errors: device_token.errors.full_messages
       }, status: :unprocessable_entity
-
     end
-
   end
 end
