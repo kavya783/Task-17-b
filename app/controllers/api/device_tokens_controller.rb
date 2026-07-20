5a6c7e9922db06229d557b0d60f46835363d2a26
kavya <kavya.actimize@gmail.com>
2026-07-20 12:40:48 +0530

    add

--- app/controllers/api/device_tokens_controller.rb
+++ app/controllers/api/device_tokens_controller.rb
@@ -2,23 +2,25 @@ class Api::DeviceTokensController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    
-    DeviceToken.where(user_id: params[:user_id]).delete_all
 
-    # save latest device token
-    device_token = DeviceToken.create(
-      user_id: params[:user_id],
-      token: params[:token]
+    device_token = DeviceToken.find_or_initialize_by(
+      user_id: params[:user_id]
     )
 
-    if device_token.persisted?
+    device_token.token = params[:token]
+
+    if device_token.save
+
       render json: {
-        message: "Latest device token saved"
+        message: "Device token saved"
       }
+
     else
+
       render json: {
         errors: device_token.errors.full_messages
       }, status: 422
+
     end
 
   end
