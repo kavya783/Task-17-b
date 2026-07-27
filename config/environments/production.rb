fd3481a85a88c5dafe29b5cdd275dfb77b514f30
kavya <kavya.actimize@gmail.com>
2026-07-27 17:34:46 +0530

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,9 @@ config.action_mailer.default_url_options = {
   protocol: "https"
 }
 
-
+config.action_mailer.default_options = {
+  from: ENV["MAIL_USERNAME"]
+}
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
   #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
