45f0deb8aa2ac970b089f1c1007d32a7dc4c1608
kavya <kavya.actimize@gmail.com>
2026-07-27 11:54:05 +0530

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,14 +72,16 @@ config.action_mailer.smtp_settings = {
   user_name: ENV["MAIL_USERNAME"],
   password: ENV["MAIL_PASSWORD"],
   authentication: :plain,
-  enable_starttls_auto: true
+  enable_starttls_auto: true,
+  open_timeout: 10,
+  read_timeout: 10
 }
 config.action_mailer.default_url_options = {
   host: "task-17-b.onrender.com",
   protocol: "https"
 }
  config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
+config.action_mailer.raise_delivery_errors = false
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
