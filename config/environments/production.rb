a9a1fadbdb88c23a6d5652275d1ecc8fc79fb9b9
kavya <kavya.actimize@gmail.com>
2026-07-27 15:16:34 +0530

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ config.active_job.queue_adapter = :async
 puts "MAIL USER: #{ENV['MAIL_USERNAME']}"
 puts "MAIL PASSWORD PRESENT: #{ENV['MAIL_PASSWORD'].present?}"
 
+
+
 config.action_mailer.delivery_method = :smtp
 
 config.action_mailer.smtp_settings = {
@@ -73,11 +75,12 @@ config.action_mailer.smtp_settings = {
   domain: ENV["SMTP_DOMAIN"],
   user_name: ENV["SMTP_USERNAME"],
   password: ENV["SMTP_PASSWORD"],
-  authentication: :plain,
+  authentication: :login,
   enable_starttls_auto: true,
   open_timeout: 10,
   read_timeout: 10
 }
+
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 
