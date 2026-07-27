ca631b45bf788aba7f9e8ecfc11df06a87d36ea1
kavya <kavya.actimize@gmail.com>
2026-07-27 17:10:32 +0530

    add

--- app/controllers/api/hrs_controller.rb
+++ app/controllers/api/hrs_controller.rb
@@ -48,7 +48,7 @@ module Api
   if hr.save
 
     begin
-   UserMailer.hr_created(hr).deliver_later
+  UserMailer.hr_created(user).deliver_now
       Rails.logger.info "MAIL SENT SUCCESSFULLY"
     rescue => e
       Rails.logger.error "CLASS: #{e.class}"
@@ -120,7 +120,7 @@ end
 UserMailer.hr_deleted(
   hr_email,
   hr_name
-).deliver_later
+).deliver_now
 rescue => e
   Rails.logger.error "HR DELETE MAIL ERROR: #{e.message}"
 end
