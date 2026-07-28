3d77729a6011cbb38658c018c47174f4c249b387
kavya <kavya.actimize@gmail.com>
2026-07-28 11:26:15 +0530

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,10 +73,10 @@ config.action_mailer.smtp_settings = {
   domain: "gmail.com",
   user_name: ENV["MAIL_USERNAME"],
   password: ENV["MAIL_PASSWORD"],
-  authentication: "plain",
+  authentication: :plain,
   enable_starttls_auto: true,
-  open_timeout: 10,
-  read_timeout: 10
+  open_timeout: 30,
+  read_timeout: 30
 }
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
