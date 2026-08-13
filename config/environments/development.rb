ff5142995e836488f33329dc049a79f3e22a2a8e
kavya <kavya.actimize@gmail.com>
2026-08-13 12:12:56 +0530

    add

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
 config.active_job.queue_adapter = :async
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
   
 
   # Don't care if the mailer can't send.
