6903cb448f62fa1d8302756052ee35e711c1754f
kavya <kavya.actimize@gmail.com>
2026-08-19 10:57:06 +0530

    add

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -210,12 +210,15 @@ end
     end
 
     # Profile image URL
-    def profile_image_url(user)
-      return nil unless user.profile_image.attached?
+   def profile_image_url(user)
+  return nil unless user.profile_image.attached?
 
-      url_for(user.profile_image)
-    end
+  variant = user.profile_image.variant(
+    resize_to_fill: [100, 100]
+  ).processed
 
+  url_for(variant)
+end
     # Compress and attach profile image
     def attach_compressed_image(user)
       compressed = ImageCompressionService.compress(
